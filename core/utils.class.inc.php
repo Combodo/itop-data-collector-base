34aea74b83d2656714d140b818ed0db499f87cba
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2021-05-28 11:39:34 +0200

    Add php version to be used.
    Co-authored-by: Pierre Goiffon <pierre.goiffon@combodo.com>

--- core/utils.class.inc.php
+++ core/utils.class.inc.php
@@ -439,7 +439,7 @@ class Utils
 
 	/**
 	 * Pretty print a JSON formatted string. Copied/pasted from http://stackoverflow.com/questions/6054033/pretty-printing-json-with-php
-	 * @deprecated 1.3.0 use `json_encode($value, JSON_PRETTY_PRINT);` instead
+	 * @deprecated 1.3.0 use `json_encode($value, JSON_PRETTY_PRINT);` instead (PHP 5.4.0 required)
 	 * @param string $json A JSON formatted object definition
 	 * @return string The nicely formatted JSON definition
 	 */
