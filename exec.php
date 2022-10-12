c59d542b11f7464cc926674128093b3f77bffd1c
xtophe38 <christophe.naud@gmail.com>
2022-10-12 10:04:15 +0200

    Add new class to the require_once list

--- exec.php
+++ exec.php
@@ -25,6 +25,7 @@ require_once(APPROOT.'core/utils.class.inc.php');
 require_once(APPROOT.'core/restclient.class.inc.php');
 require_once(APPROOT.'core/lookuptable.class.inc.php');
 require_once(APPROOT.'core/mappingtable.class.inc.php');
+require_once(APPROOT.'core/collectionplan.class.inc.php');
 require_once(APPROOT.'core/collector.class.inc.php');
 require_once(APPROOT.'core/orchestrator.class.inc.php');
 require_once(APPROOT.'core/sqlcollector.class.inc.php'); // Depends on Orchestrator for settings a minimum version for PHP because of the use of PDO
