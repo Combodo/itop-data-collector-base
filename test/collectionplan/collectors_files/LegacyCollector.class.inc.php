e1997a8c405bebf24b4db0d275ff8c650a4fdc93
xtophe38 <christophe.naud@gmail.com>
2022-10-26 11:52:17 +0200

    Create tests for CollectionPlan class

---
+++ test/collectionplan/collectors_files/LegacyCollector.class.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+class LegacyCollector extends Collector
+{
+	public function CheckToLaunch(): bool
+	{
+		return false;
+	}
+}
---
+++ test/collectionplan/collectors_files/src/StandardCollector.class.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+class StandardCollector extends Collector
+{
+	
+}
