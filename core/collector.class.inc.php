a4334174d5509f2acd98b90ad2197ff4aac343ea
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2022-12-06 17:50:10 +0100

    Fixed warning when aSkippedAttributes is not correctly populated (#11)

--- core/collector.class.inc.php
+++ core/collector.class.inc.php
@@ -513,7 +513,7 @@ abstract class Collector
 		$this->aCSVHeaders = array();
 		foreach ($aHeaders as $sHeader) {
 			if (($sHeader != 'primary_key') && !array_key_exists($sHeader, $this->aFields)) {
-				if (!in_array($sHeader, $this->aSkippedAttributes)) {
+				if (!$this->AttributeIsOptional($sHeader)) {
 					Utils::Log(LOG_WARNING, "Invalid column '$sHeader', will be ignored.");
 				}
 			} else {
