Combodo/itop-data-collector-base

commit 595abed7e894b847a8a829d41f435de20c281eaf

N°3214 - Collector JSON

toolkit/dump_tasks.php
@@ -98,7 +98,13 @@ else
 			// Replace some litterals by their usual placeholders
 			$aCurrentTaskDefinition['user_id'] = '$synchro_user$';
 			$aCurrentTaskDefinition['notify_contact_id'] = '$contact_to_notify$';
-			
+
+
+            if( isset($aCurrentTaskDefinition['database_table_name']))
+            {
+                $aCurrentTaskDefinition['database_table_name'] = "";
+            }
+
 			$sDefinition = json_encode($aCurrentTaskDefinition);
 			echo Utils::JSONPrettyPrint($sDefinition)."\n";
 		}

conf/params.distrib.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!-- Default values for parameters. Do NOT alter this file, use params.local.xml instead -->
 <parameters>
-  <itop_url>http://localhost/iTop</itop_url>
+  <itop_url>https://localhost/iTop</itop_url>
   <itop_login>admin</itop_login>
   <itop_password>admin</itop_password>
   

core/collector.class.inc.php
@@ -271,25 +271,27 @@ abstract class Collector
 		{
 			// No module found, use a default value...
 			$this->sVersion = '1.0.0';
+            Utils::Log(LOG_WARNING, "Please create a 'module.*.php' file in 'collectors' folder in order to define the version of collectors");
 		}
-		
-		try
+		else
 		{
-			$sModuleFileContents = file_get_contents($sModuleFile);
-			$sModuleFileContents = str_replace(array('<?php', '?>'), '', $sModuleFileContents);
-			$sModuleFileContents = str_replace('SetupWebPage::AddModule(', '$this->InitVersionCallback(', $sModuleFileContents);
-			$bRet = eval($sModuleFileContents);
-			
-			if ($bRet === false)
-			{
-				Utils::Log(LOG_WARNING, "Eval of '$sModuleFileContents' returned false");
-			}
-		}
-		catch(Exception $e)
-		{
-			// Continue...
-			Utils::Log(LOG_WARNING, "Eval of '$sModuleFileContents' caused an exception: ".$e->getMessage());
-		}		
+            try
+            {
+                $sModuleFileContents = file_get_contents($sModuleFile);
+                $sModuleFileContents = str_replace(array('<?php', '?>'), '', $sModuleFileContents);
+                $sModuleFileContents = str_replace('SetupWebPage::AddModule(', '$this->InitVersionCallback(', $sModuleFileContents);
+                $bRet = eval($sModuleFileContents);
+
+                if ($bRet === false) {
+                    Utils::Log(LOG_WARNING, "Eval of '$sModuleFileContents' returned false");
+                }
+            }
+            catch (Exception $e)
+            {
+                // Continue...
+                Utils::Log(LOG_WARNING, "Eval of '$sModuleFileContents' caused an exception: " . $e->getMessage());
+            }
+        }
 	}
 	
 	/**

core/csvcollector.class.inc.php
@@ -76,32 +76,32 @@ abstract class CSVCollector extends Collector
         Utils::Log(LOG_INFO, "[".get_class($this)."] CLI command used is [". $this->sCsvCliCommand . "]");
 
         // Read the SQL query from the configuration
-        $csvFilePath = Utils::GetConfigurationValue(get_class($this)."_csv", '');
-        if ($csvFilePath == '')
+        $sCsvFilePath = Utils::GetConfigurationValue(get_class($this)."_csv", '');
+        if ($sCsvFilePath == '')
         {
             // Try all lowercase
-            $csvFilePath = Utils::GetConfigurationValue(strtolower(get_class($this))."_csv", '');
+            $sCsvFilePath = Utils::GetConfigurationValue(strtolower(get_class($this))."_csv", '');
         }
-        if ($csvFilePath == '')
+        if ($sCsvFilePath == '')
         {
             // No query at all !!
             Utils::Log(LOG_ERR, "[".get_class($this)."] no CSV file configured! Cannot collect data. The csv was expected to be configured as '".strtolower(get_class($this))."_csv' in the configuration file.");
             return false;
         }
 
-        if (!is_file($csvFilePath))
+        if (!is_file($sCsvFilePath))
         {
-            Utils::Log(LOG_INFO, "[".get_class($this)."] CSV file not found in [". $csvFilePath . "]");
-            $csvFilePath = APPROOT . $csvFilePath;
-            if (!is_file($csvFilePath)) {
-                Utils::Log(LOG_ERR, "[" . get_class($this) . "] Cannot find CSV file $csvFilePath");
+            Utils::Log(LOG_INFO, "[".get_class($this)."] CSV file not found in [". $sCsvFilePath . "]");
+            $sCsvFilePath = APPROOT . $sCsvFilePath;
+            if (!is_file($sCsvFilePath)) {
+                Utils::Log(LOG_ERR, "[" . get_class($this) . "] Cannot find CSV file $sCsvFilePath");
                 return false;
             }
         }
 
-        if (!is_readable($csvFilePath))
+        if (!is_readable($sCsvFilePath))
         {
-            Utils::Log(LOG_ERR, "[".get_class($this)."] Cannot read CSV file $csvFilePath");
+            Utils::Log(LOG_ERR, "[".get_class($this)."] Cannot read CSV file $sCsvFilePath");
             return false;
         }
 
@@ -110,9 +110,9 @@ abstract class CSVCollector extends Collector
             $this->Exec($this->sCsvCliCommand);
         }
 
-        $hHandle = fopen($csvFilePath, "r");
+        $hHandle = fopen($sCsvFilePath, "r");
         if (!$hHandle) {
-            Utils::Log(LOG_ERR, "[" . get_class($this) . "] Handle issue with file $csvFilePath");
+            Utils::Log(LOG_ERR, "[" . get_class($this) . "] Handle issue with file $sCsvFilePath");
             return false;
         }
 
@@ -121,7 +121,7 @@ abstract class CSVCollector extends Collector
         }
 
         fclose($hHandle);
-        $this->idx = 0;
+        $this->iIdx = 0;
 		return $bRet;
     }
 
@@ -133,31 +133,31 @@ abstract class CSVCollector extends Collector
      * @return string[] - Array with keys: 'code' - exit code, 'out' - stdout, 'err' - stderr
      * @throws \Exception
      */
-    function Exec($cmd) {
+    function Exec($sCmd) {
         $iBeginTime = time();
-        $workdir = APPROOT;
-        $descriptorspec = array(
+        $sWorkDir = APPROOT;
+        $aDescriptorSpec = array(
             0 => array("pipe", "r"),  // stdin
             1 => array("pipe", "w"),  // stdout
             2 => array("pipe", "w"),  // stderr
         );
-        $process = proc_open($cmd, $descriptorspec, $pipes, $workdir, null);
+        $rProcess = proc_open($sCmd, $aDescriptorSpec, $aPipes, $sWorkDir, null);
 
-        $stdout = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
+        $sStdOut = stream_get_contents($aPipes[1]);
+        fclose($aPipes[1]);
 
-        $stderr = stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
+        $sStdErr = stream_get_contents($aPipes[2]);
+        fclose($aPipes[2]);
 
-        $code = proc_close($process);
+        $iCode = proc_close($rProcess);
 
         $iElapsed = time() - $iBeginTime;
-        Utils::Log(LOG_INFO, "Command: $cmd. Workdir: $workdir");
-        if (0 === $code) {
-            Utils::Log(LOG_INFO, "elapsed:${iElapsed}s output: $stdout");
-            return $stdout;
+        Utils::Log(LOG_INFO, "Command: $sCmd. Workdir: $sWorkDir");
+        if (0 === $iCode) {
+            Utils::Log(LOG_INFO, "elapsed:${iElapsed}s output: $sStdOut");
+            return $sStdOut;
         } else {
-            throw new Exception("Command failed : $cmd \n\t\t=== with status:$code \n\t\t=== stderr:$stderr \n\t\t=== stdout: $stdout");
+            throw new Exception("Command failed : $sCmd \n\t\t=== with status:$iCode \n\t\t=== stderr:$sStdErr \n\t\t=== stdout: $sStdOut");
         }
     }
 
@@ -204,16 +204,16 @@ abstract class CSVCollector extends Collector
      */
     public function Fetch()
     {
-        if ($this->idx >= sizeof($this->aCsvLines))
+        if ($this->iIdx >= sizeof($this->aCsvLines))
         {
             return false;
         }
 
-        /** NextLineObject**/ $next_line_arr = $this->get_next_line();
+        /** NextLineObject**/ $oNextLineArr = $this->getNextLine();
 
         if (! $this->aColumns)
         {
-            $aChecks = $this->CheckSQLCsvHeaders($next_line_arr->getValues());
+            $aChecks = $this->CheckSQLCsvHeaders($oNextLineArr->getValues());
             foreach($aChecks['errors'] as $sError)
             {
                 Utils::Log(LOG_ERR, "[".get_class($this)."] $sError");
@@ -226,50 +226,50 @@ abstract class CSVCollector extends Collector
             {
                 throw new Exception("Missing columns in the CSV file.");
             }
-            $this->aColumns = array_merge($next_line_arr->getValues());
-            $this->idx++;
+            $this->aColumns = array_merge($oNextLineArr->getValues());
+            $this->iIdx++;
         }
 
-        /** NextLineObject**/ $next_line_arr = $this->get_next_line();
-        $column_size = sizeof($this->aColumns);
-        $line_size = sizeof($next_line_arr->getValues());
-        if ($column_size !== $line_size)
+        /** NextLineObject**/ $oNextLineArr = $this->getNextLine();
+        $iColumnSize = sizeof($this->aColumns);
+        $iLineSize = sizeof($oNextLineArr->getValues());
+        if ($iColumnSize !== $iLineSize)
         {
-            $line = $this->idx + 1;
-            Utils::Log(LOG_ERR, "[" . get_class($this) . "] Wrong number of columns ($line_size) on line $line (expected $column_size columns just like in header): " . $next_line_arr->getCsvLine());
+            $line = $this->iIdx + 1;
+            Utils::Log(LOG_ERR, "[" . get_class($this) . "] Wrong number of columns ($iLineSize) on line $line (expected $iColumnSize columns just like in header): " . $oNextLineArr->getCsvLine());
             throw new Exception("Invalid CSV file.");
         }
 
         $aData = array();
         $i=0;
-        foreach ($next_line_arr->getValues() as $val)
+        foreach ($oNextLineArr->getValues() as $sVal)
         {
             $column = $this->aColumns[$i];
             if (!array_key_exists($column, $this->aSkippedAttributes))
             {
-                $aData[$column] = $val;
+                $aData[$column] = $sVal;
             }
             $i++;
         }
 
-        $this->idx++;
+        $this->iIdx++;
         return $aData;
     }
 
     /**
      * @return NextLineObject
      */
-    public function get_next_line()
+    public function getNextLine()
     {
-        $csv_line = $this->aCsvLines[$this->idx];
-        $aValues = explode($this->sCsvSeparator, $csv_line);
-        return new NextLineObject($csv_line, $aValues);
+        $sCsvLine = $this->aCsvLines[$this->iIdx];
+        $aValues = explode($this->sCsvSeparator, $sCsvLine);
+        return new NextLineObject($sCsvLine, $aValues);
     }
 }
 
 class NextLineObject
 {
-    private $csv_line;
+    private $sCsvLine;
     private $aValues;
 
     /**
@@ -277,9 +277,9 @@ class NextLineObject
      * @param $csv_line
      * @param $aValues
      */
-    public function __construct($csv_line, $aValues)
+    public function __construct($sCsvLine, $aValues)
     {
-        $this->csv_line = $csv_line;
+        $this->sCsvLine = $sCsvLine;
         $this->aValues = $aValues;
     }
 
@@ -288,7 +288,7 @@ class NextLineObject
      */
     public function getCsvLine()
     {
-        return $this->csv_line;
+        return $this->sCsvLine;
     }
 
     /**

core/utils.class.inc.php
@@ -17,6 +17,7 @@
 define('LOG_NONE', -1);
 
 define('CONF_DIR', APPROOT.'conf/');
+require_once(APPROOT.'core/ioexception.class.inc.php');
 
 class Utils
 {
@@ -25,7 +26,7 @@ class Utils
 	static protected $oConfig = null;
 	static protected $aConfigFiles = array();
 
-    static protected $mocked_logger;
+    static protected $oMockedLogger;
 
 	static public function ReadParameter($sParamName, $defaultValue)
 	{
@@ -106,11 +107,11 @@ class Utils
 	static public function Log($iPriority, $sMessage)
 	{
         //testing only LOG_ERR
-        if (self::$mocked_logger)
+        if (self::$oMockedLogger)
         {
             if ($iPriority <= self::$iConsoleLogLevel && $iPriority <= LOG_ERR)
             {
-                self::$mocked_logger->Log($iPriority, $sMessage);
+                self::$oMockedLogger->Log($iPriority, $sMessage);
             }
         }
 
@@ -163,9 +164,9 @@ class Utils
 		}
 	}
 
-	static public function mock_log($mocked_logger)
+	static public function MockLog($oMockedLogger)
     {
-        self::$mocked_logger = $mocked_logger;
+        self::$oMockedLogger = $oMockedLogger;
     }
 
 	static public function LoadConfig()
@@ -275,7 +276,7 @@ class Utils
 	
 		if (function_exists('curl_init'))
 		{
-			// If cURL is available, let's use it, since it provides a greater control over the various HTTP/SSL options
+            // If cURL is available, let's use it, since it provides a greater control over the various HTTP/SSL options
 			// For instance fopen does not allow to work around the bug: http://stackoverflow.com/questions/18191672/php-curl-ssl-routinesssl23-get-server-helloreason1112
 			// by setting the SSLVERSION to 3 as done below.
 			$aHTTPHeaders = array();
@@ -317,10 +318,15 @@ class Utils
 			$iErr = curl_errno($ch);
 			$sErrMsg = curl_error( $ch );
 			$aHeaders = curl_getinfo( $ch );
-			if ($iErr !== 0)
+            Utils::Log(LOG_DEBUG, 'aHeaders'.json_encode($aHeaders));
+            if ($iErr !== 0)
 			{
 				throw new IOException("Problem opening URL: $sUrl, $sErrMsg");
 			}
+            if ( isset($aHeaders["http_code"]) && $aHeaders["http_code"] >= 400 )
+            {
+                throw new IOException("Problem opening URL: $sUrl. Code HTTP: ".$aHeaders["http_code"]);
+            }
 			if (is_array($aResponseHeaders))
 			{
 				$aHeaders = curl_getinfo($ch);
@@ -335,8 +341,7 @@ class Utils
 		else
 		{
 			// cURL is not available let's try with streams and fopen...
-				
-			$sData = http_build_query($aData);
+            $sData = http_build_query($aData);
 			$aParams = array('http' => array(
 				'method' => 'POST',
 				'content' => $sData,

test/csvcollector_test.inc.php
@@ -22,13 +22,13 @@ class TestCsvCollector extends TestCase
         parent::setUp();
 
         $aCollectorFiles = glob(TestCsvCollector::$sCollectorPath . "*");
-        foreach ($aCollectorFiles as $file)
+        foreach ($aCollectorFiles as $sFile)
         {
-            unlink($file);
+            unlink($sFile);
         }
 
         $this->oMockedLogger = $this->createMock("UtilsLogger");
-        \Utils::mock_log($this->oMockedLogger);
+        \Utils::MockLog($this->oMockedLogger);
 
     }
 
@@ -36,9 +36,9 @@ class TestCsvCollector extends TestCase
     {
         parent::tearDown();
         $aCollectorFiles = glob(TestCsvCollector::$sCollectorPath . "*");
-        foreach ($aCollectorFiles as $file)
+        foreach ($aCollectorFiles as $sFile)
         {
-            unlink($file);
+            unlink($sFile);
         }
     }
 
@@ -48,29 +48,29 @@ class TestCsvCollector extends TestCase
         $this->assertTrue(is_a(new \IOException(""), "IOException"));
     }
 
-    private function copy($pattern)
+    private function copy($sPattern)
     {
         if (! is_dir(TestCsvCollector::$sCollectorPath))
         {
             mkdir(TestCsvCollector::$sCollectorPath);
         }
 
-        $aFiles = glob($pattern);
-        foreach ($aFiles as $file)
+        $aFiles = glob($sPattern);
+        foreach ($aFiles as $sFile)
         {
-            if (is_file($file))
+            if (is_file($sFile))
             {
-                $bRes = copy($file, TestCsvCollector::$sCollectorPath . basename($file));
+                $bRes = copy($sFile, TestCsvCollector::$sCollectorPath . basename($sFile));
                 if (!$bRes)
                 {
-                    throw new \Exception("Failed copying $file to " . TestCsvCollector::$sCollectorPath . basename($file));
+                    throw new \Exception("Failed copying $sFile to " . TestCsvCollector::$sCollectorPath . basename($sFile));
                 }
             }
         }
     }
 
     /**
-     * @param bool $additional_dir
+     * @param bool $sAdditionalDir
      * @dataProvider OrgCollectorProvider
      * @throws \Exception
      */
@@ -84,10 +84,10 @@ class TestCsvCollector extends TestCase
         $this->oMockedLogger->expects($this->exactly(0))
             ->method("Log");
 
-        $orgCollector = new \iTopPersonCsvCollector();
+        $oOrgCollector = new \iTopPersonCsvCollector();
         \Utils::LoadConfig();
 
-        $this->assertTrue($orgCollector->Collect());
+        $this->assertTrue($oOrgCollector->Collect());
 
         $sExpected_content = file_get_contents(TestCsvCollector::$sCollectorPath ."expected_generated.csv");
 
@@ -112,9 +112,9 @@ class TestCsvCollector extends TestCase
         mkdir($sTargetDir);
         $sTargetDir = realpath($sTargetDir);
         $sContent = str_replace("TMPDIR", $sTargetDir, file_get_contents(APPROOT . "/test/single_csv/absolutepath/params.distrib.xml"));
-        $hHandle  = fopen(APPROOT . "/collectors/params.distrib.xml", "w");
-        fwrite($hHandle , $sContent);
-        fclose($hHandle );
+        $rHandle  = fopen(APPROOT . "/collectors/params.distrib.xml", "w");
+        fwrite($rHandle , $sContent);
+        fclose($rHandle );
 
         $sCsvFile = dirname(__FILE__) . "/single_csv/nominal/iTopPersonCsvCollector.csv";
         if (is_file($sCsvFile))
@@ -148,19 +148,19 @@ class TestCsvCollector extends TestCase
      * @throws \Exception
      * @dataProvider ErrorFileProvider
      */
-    public function testCsvErrors($sError_file, $sError_msg, $sException_msg=false)
+    public function testCsvErrors($sErrorFile, $sErrorMsg, $sExceptionMsg=false)
     {
         $this->copy(APPROOT . "/test/single_csv/common/*");
-        copy(APPROOT . "/test/single_csv/csv_errors/$sError_file", TestCsvCollector::$sCollectorPath . "iTopPersonCsvCollector.csv");
+        copy(APPROOT . "/test/single_csv/csv_errors/$sErrorFile", TestCsvCollector::$sCollectorPath . "iTopPersonCsvCollector.csv");
 
         require_once TestCsvCollector::$sCollectorPath . "iTopPersonCsvCollector.class.inc.php";
         $orgCollector = new \iTopPersonCsvCollector();
         \Utils::LoadConfig();
 
-        if ($sException_msg) {
+        if ($sExceptionMsg) {
             $this->oMockedLogger->expects($this->exactly(2))
                 ->method("Log")
-                ->withConsecutive(array(LOG_ERR, $sError_msg), array(LOG_ERR, $sException_msg));
+                ->withConsecutive(array(LOG_ERR, $sErrorMsg), array(LOG_ERR, $sExceptionMsg));
         }
         else{
             $this->oMockedLogger->expects($this->exactly(0))
@@ -169,10 +169,10 @@ class TestCsvCollector extends TestCase
         try{
             $bResult = $orgCollector->Collect();
 
-            $this->assertEquals($sException_msg ? false : true, $bResult);
+            $this->assertEquals($sExceptionMsg ? false : true, $bResult);
         }
         catch(Exception $e){
-            $this->assertEquals($sException_msg, $e->getMessage());
+            $this->assertEquals($sExceptionMsg, $e->getMessage());
         }
     }
 

test/jsoncollector_test.inc.php
@@ -22,13 +22,13 @@ class TestJsonCollector extends TestCase
         parent::setUp();
 
         $aCollectorFiles = glob(TestJsonCollector::$sCollectorPath . "*");
-        foreach ($aCollectorFiles as $file)
+        foreach ($aCollectorFiles as $fFile)
         {
-            unlink($file);
+            unlink($fFile);
         }
 
         $this->oMockedLogger = $this->createMock("UtilsLogger");
-        \Utils::mock_log($this->oMockedLogger);
+        \Utils::MockLog($this->oMockedLogger);
 
     }
 
@@ -56,14 +56,14 @@ class TestJsonCollector extends TestCase
         }
 
         $aFiles = glob($sPattern);
-        foreach ($aFiles as $file)
+        foreach ($aFiles as $fFile)
         {
-            if (is_file($file))
+            if (is_file($fFile))
             {
-                $bRes = copy($file, TestJsonCollector::$sCollectorPath . basename($file));
+                $bRes = copy($fFile, TestJsonCollector::$sCollectorPath . basename($fFile));
                 if (!$bRes)
                 {
-                    throw new \Exception("Failed copying $file to " . TestJsonCollector::COLLECTOR_PATH . basename($file));
+                    throw new \Exception("Failed copying $fFile to " . TestJsonCollector::COLLECTOR_PATH . basename($fFile));
                 }
             }
         }
@@ -75,9 +75,9 @@ class TestJsonCollector extends TestCase
     {
         $sContent = str_replace("APPROOT", APPROOT, file_get_contents(APPROOT . $initDir."/params.distrib.xml"));
         print_r($sContent);
-        $hHandle  = fopen(APPROOT . "/collectors/params.distrib.xml", "w");
-        fwrite($hHandle , $sContent);
-        fclose($hHandle );
+        $rHandle  = fopen(APPROOT . "/collectors/params.distrib.xml", "w");
+        fwrite($rHandle , $sContent);
+        fclose($rHandle );
 
     }
     /**
@@ -96,10 +96,10 @@ class TestJsonCollector extends TestCase
         $this->oMockedLogger->expects($this->exactly(0))
             ->method("Log");
 
-        $orgCollector = new \ITopPersonJsonCollector();
+        $oOrgCollector = new \ITopPersonJsonCollector();
         \Utils::LoadConfig();
 
-        $this->assertTrue($orgCollector->Collect());
+        $this->assertTrue($oOrgCollector->Collect());
 
         $sExpected_content = file_get_contents(TestJsonCollector::$sCollectorPath ."expected_generated.csv");
 
@@ -122,7 +122,7 @@ class TestJsonCollector extends TestCase
      * @throws \Exception
      * @dataProvider ErrorFileProvider
      */
-    public function testJsonErrors($sAdditionalDir, $sError_msg, $sException_msg=false, $sException_msg3=false)
+    public function testJsonErrors($sAdditionalDir, $sErrorMsg, $sExceptionMsg=false, $sExceptionMsg3=false)
     {
         $this->copy(APPROOT . "/test/single_json/common/*");
         $this->copy(APPROOT . "/test/single_json/json_error/".$sAdditionalDir."/*");
@@ -133,20 +133,20 @@ class TestJsonCollector extends TestCase
         $oOrgCollector = new \ITopPersonJsonCollector();
         \Utils::LoadConfig();
 
-        if ($sException_msg3) {
+        if ($sExceptionMsg3) {
             $this->oMockedLogger->expects($this->exactly(3))
                 ->method("Log")
-                ->withConsecutive(array(LOG_ERR, $sError_msg), array(LOG_ERR, $sException_msg), array(LOG_ERR, $sException_msg3));
+                ->withConsecutive(array(LOG_ERR, $sErrorMsg), array(LOG_ERR, $sExceptionMsg), array(LOG_ERR, $sExceptionMsg3));
         }
-        elseif ($sException_msg) {
+        elseif ($sExceptionMsg) {
             $this->oMockedLogger->expects($this->exactly(2))
                 ->method("Log")
-                ->withConsecutive(array(LOG_ERR, $sError_msg), array(LOG_ERR, $sException_msg));
+                ->withConsecutive(array(LOG_ERR, $sErrorMsg), array(LOG_ERR, $sExceptionMsg));
         }
-        elseif ($sError_msg) {
+        elseif ($sErrorMsg) {
             $this->oMockedLogger->expects($this->exactly(1))
                 ->method("Log")
-                ->withConsecutive(array(LOG_ERR, $sError_msg));
+                ->withConsecutive(array(LOG_ERR, $sErrorMsg));
         }
         else {
             $this->oMockedLogger->expects($this->exactly(0))
@@ -155,10 +155,10 @@ class TestJsonCollector extends TestCase
         try{
             $bResult = $oOrgCollector->Collect();
 
-            $this->assertEquals($sError_msg ? false : true, $bResult);
+            $this->assertEquals($sErrorMsg ? false : true, $bResult);
         }
         catch(Exception $e){
-             $this->assertEquals($sException_msg, $e->getMessage());
+             $this->assertEquals($sExceptionMsg, $e->getMessage());
         }
     }
 

commit fc381ee2fb0dc0f262a258211b4bde30a4b77bdc

Fix datatable name removed in dump_task Regression introduced in 595abed

toolkit/dump_tasks.php
@@ -92,11 +92,6 @@ if ($sTaskName == '*') {
 				$aCurrentTaskDefinition['user_id'] = '$synchro_user$';
 				$aCurrentTaskDefinition['notify_contact_id'] = '$contact_to_notify$';
 
-
-				if (isset($aCurrentTaskDefinition['database_table_name'])) {
-					$aCurrentTaskDefinition['database_table_name'] = "";
-				}
-
 				echo json_encode($aCurrentTaskDefinition, JSON_PRETTY_PRINT);
 			}
 		} else {