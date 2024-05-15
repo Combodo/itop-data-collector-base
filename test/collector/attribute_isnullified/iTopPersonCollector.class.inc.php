8cc66f7c75b46a9f3fdfc2849acf3cd4f42b6723
odain <olivier.dain@combodo.com>
2024-05-15 15:25:56 +0200

    N°5139 - Let data that is null also be null at the other side-fix

--- test/collector/attribute_isnullified/iTopPersonCollector.class.inc.php
+++ test/collector/attribute_isnullified/iTopPersonCollector.class.inc.php
@@ -2,29 +2,62 @@
 
 class iTopPersonCollector extends Collector
 {
-	private $bFetched;
+	private $i;
+	private $aCurrentData;
 
 	protected function Fetch()
 	{
-		if (! $this->bFetched) {
-			$this->bFetched = true;
-			return [
-				'primary_key' => 1,
-				'first_name' => "isaac",
-				'name' => "asimov",
-				'org_id' => "Demo",
-				'phone' => null,
-				'mobile_phone' => "123456",
-				'employee_number' => "9998877665544",
-				'email' => "issac.asimov@function.io",
-				'function' => "writer",
-				'Status' => "Active",
+		if (is_null($this->aCurrentData)){
+			$this->i = 0;
+			$this->aCurrentData = [
+				[
+					'primary_key' => 1,
+					'first_name' => "isaac",
+					'name' => "asimov_null",
+					'org_id' => "Demo",
+					'phone' => null,
+					'mobile_phone' => "123456",
+					'employee_number' => "9998877665544",
+					'email' => "issac.asimov@function.io",
+					'function' => "writer",
+					'Status' => "Active",
+				],
+				[
+					'primary_key' => 2,
+					'first_name' => "isaac",
+					'name' => "asimov_empty",
+					'org_id' => "Demo",
+					'phone' => "",
+					'mobile_phone' => "123456",
+					'employee_number' => "9998877665544",
+					'email' => "issac.asimov@function.io",
+					'function' => "writer",
+					'Status' => "Active",
+				],
+				[
+					'primary_key' => 3,
+					'first_name' => "isaac",
+					'name' => "asimov_notempty",
+					'org_id' => "Demo",
+					'phone' => "not empty",
+					'mobile_phone' => "123456",
+					'employee_number' => "9998877665544",
+					'email' => "issac.asimov@function.io",
+					'function' => "writer",
+					'Status' => "Active",
+				],
 			];
 		}
 
-		return null;
+		$res = null;
+		if ($this->i < count($this->aCurrentData)){
+			$res=$this->aCurrentData[$this->i];
+			$this->i++;
+		}
+
+		return $res;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * @see Collector::AttributeIsOptional()
