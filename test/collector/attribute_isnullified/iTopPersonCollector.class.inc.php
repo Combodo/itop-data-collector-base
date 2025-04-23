0f856a69202c6445b192931a9b1d241c25cfeb4d
odain <olivier.dain@combodo.com>
2025-04-23 09:58:14 +0200

    N°5139 - test small cleanup

--- test/collector/attribute_isnullified/iTopPersonCollector.class.inc.php
+++ test/collector/attribute_isnullified/iTopPersonCollector.class.inc.php
@@ -5,55 +5,53 @@ class iTopPersonCollector extends Collector
 	private $i;
 	private $aCurrentData;
 
+	public function __construct() {
+		parent::__construct();
+
+		$this->i = 0;
+		$this->aCurrentData = [
+			[
+				'primary_key' => 1,
+				'first_name' => "isaac",
+				'name' => "asimov_null",
+				'org_id' => "Demo",
+				'phone' => null,
+				'mobile_phone' => "123456",
+				'employee_number' => "9998877665544",
+				'email' => "issac.asimov@function.io",
+				'function' => "writer",
+				'Status' => "Active",
+			],
+			[
+				'primary_key' => 2,
+				'first_name' => "isaac",
+				'name' => "asimov_empty",
+				'org_id' => "Demo",
+				'phone' => "",
+				'mobile_phone' => "123456",
+				'employee_number' => "9998877665544",
+				'email' => "issac.asimov@function.io",
+				'function' => "writer",
+				'Status' => "Active",
+			],
+			[
+				'primary_key' => 3,
+				'first_name' => "isaac",
+				'name' => "asimov_notempty",
+				'org_id' => "Demo",
+				'phone' => "not empty",
+				'mobile_phone' => "123456",
+				'employee_number' => "9998877665544",
+				'email' => "issac.asimov@function.io",
+				'function' => "writer",
+				'Status' => "Active",
+			],
+		];
+	}
 	protected function Fetch()
 	{
-		if (is_null($this->aCurrentData)){
-			$this->i = 0;
-			$this->aCurrentData = [
-				[
-					'primary_key' => 1,
-					'first_name' => "isaac",
-					'name' => "asimov_null",
-					'org_id' => "Demo",
-					'phone' => null,
-					'mobile_phone' => "123456",
-					'employee_number' => "9998877665544",
-					'email' => "issac.asimov@function.io",
-					'function' => "writer",
-					'Status' => "Active",
-				],
-				[
-					'primary_key' => 2,
-					'first_name' => "isaac",
-					'name' => "asimov_empty",
-					'org_id' => "Demo",
-					'phone' => "",
-					'mobile_phone' => "123456",
-					'employee_number' => "9998877665544",
-					'email' => "issac.asimov@function.io",
-					'function' => "writer",
-					'Status' => "Active",
-				],
-				[
-					'primary_key' => 3,
-					'first_name' => "isaac",
-					'name' => "asimov_notempty",
-					'org_id' => "Demo",
-					'phone' => "not empty",
-					'mobile_phone' => "123456",
-					'employee_number' => "9998877665544",
-					'email' => "issac.asimov@function.io",
-					'function' => "writer",
-					'Status' => "Active",
-				],
-			];
-		}
-
-		$res = null;
-		if ($this->i < count($this->aCurrentData)){
-			$res=$this->aCurrentData[$this->i];
-			$this->i++;
-		}
+		$res = $this->aCurrentData[$this->i] ?? null;
+		$this->i++;
 
 		return $res;
 	}
