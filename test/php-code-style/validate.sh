5910d902e4541c4e448da28ed2b34ed7730cb3c5
odain <olivier.dain@combodo.com>
2025-10-17 08:48:54 +0200

    codestyle report  in verbose

--- test/php-code-style/validate.sh
+++ test/php-code-style/validate.sh
@@ -10,6 +10,6 @@ CODESTYLE_DIR=$(dirname $0)
 cd $CODESTYLE_DIR && composer install
 cd -
 
-$CODESTYLE_DIR/vendor/bin/php-cs-fixer check --config $CODESTYLE_DIR/.php-cs-fixer.dist.php --format=junit >$LOG_DIR/codestyle_results.xml
+$CODESTYLE_DIR/vendor/bin/php-cs-fixer check --config $CODESTYLE_DIR/.php-cs-fixer.dist.php -vvv --format=junit >$LOG_DIR/codestyle_results.xml
 sed -i -e 's|.*<?xml |<?xml |' $LOG_DIR/codestyle_results.xml
 ls -alh $LOG_DIR
