5a2e74e4fb8eecaecfa4f27bae7a26c80a8433d5
odain <olivier.dain@combodo.com>
2025-10-17 11:33:05 +0200

    indent with tabs + inception style applied everywhere even php-cs-fixer itself

--- test/php-code-style/.php-cs-fixer.dist.php
+++ test/php-code-style/.php-cs-fixer.dist.php
@@ -1,19 +1,22 @@
 <?php
 
-$APPROOT=dirname(__DIR__, 2);
+$APPROOT = dirname(__DIR__, 2);
 
 echo $APPROOT;
 $finder = PhpCsFixer\Finder::create()
-    ->exclude('vendor')
+	->exclude('vendor')
 	->in($APPROOT)
-    ;
+;
 
 $config = new PhpCsFixer\Config();
 return $config->setRiskyAllowed(true)
-    ->setRules([
-	    '@PSR12'       => true,
-        'no_extra_blank_lines' => true,
-	    'array_syntax' => ['syntax' => 'short'],
-    ])
-    ->setFinder($finder)
+	->setRules([
+		'@PSR12'       => true,
+		'indentation_type' => true,
+		'no_extra_blank_lines' => true,
+		'array_syntax' => ['syntax' => 'short'],
+	])
+	->setIndent("\t")
+	->setLineEnding("\n")
+	->setFinder($finder)
 ;
